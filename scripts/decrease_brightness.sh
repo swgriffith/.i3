d02ddc542564a5a7695f301ee64e9bb3c62a5570
Steve Griffith <steve.griffith@microsoft.com>
2019-07-31 12:40:37 -0400

    added manjaro i3 setup

--- scripts/decrease_brightness.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-b=$(cat /sys/class/backlight/intel_backlight/brightness)
-b=$((b-30))
-sudo echo $b | sudo tee /sys/class/backlight/intel_backlight/brightness
--- scripts/increase_brightness.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-b=$(cat /sys/class/backlight/intel_backlight/brightness)
-b=$((b+30))
-echo $b |sudo  tee /sys/class/backlight/intel_backlight/brightness
